e689d994795767bc06c36b04479f97ef101952ba
Hosang Yoon <hosang.yoon@amd.com>
2025-02-24 10:27:11 -0500

    Fix unit tests to pass on Navi
    Signed-off-by: Hosang Yoon <hosang.yoon@amd.com>

--- tests/kernels/test_attention.py
+++ tests/kernels/test_attention.py
@@ -224,7 +224,10 @@ def test_paged_attention(
 
     elif version in ("v2", "rocm"):
         if current_platform.is_rocm():
-            PARTITION_SIZE = 1024 if version == "v2" else 256
+            if is_navi():
+                PARTITION_SIZE = 512 if version == "v2" else 256
+            else:
+                PARTITION_SIZE = 1024 if version == "v2" else 256
         num_partitions = ((max_seq_len + PARTITION_SIZE - 1) // PARTITION_SIZE)
         assert PARTITION_SIZE % block_size == 0
         num_seqs, num_heads, head_size = output.shape
