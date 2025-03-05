f61ad9302d112637393818b9b4eccd8ce41a9e54
Hosang Yoon <hosang.yoon@amd.com>
2025-03-05 17:21:22 -0500

    fix unit test args for custom paged attention
    Signed-off-by: Hosang Yoon <hosang.yoon@amd.com>

--- benchmarks/kernels/benchmark_paged_attention.py
+++ benchmarks/kernels/benchmark_paged_attention.py
@@ -17,6 +17,8 @@ logger = init_logger(__name__)
 NUM_BLOCKS = 128 * 1024
 PARTITION_SIZE = 512
 PARTITION_SIZE_ROCM = 256
+GPU_ARCH = torch.cuda.get_device_properties("cuda").gcnArchName
+ON_NAVI = "gfx1" in GPU_ARCH
 
 
 @torch.inference_mode()
@@ -86,7 +88,7 @@ def main(
     if version == "v2":
         if current_platform.is_rocm():
             global PARTITION_SIZE
-            if not args.custom_paged_attn:
+            if not args.custom_paged_attn and not ON_NAVI:
                 PARTITION_SIZE = 1024
             else:
                 PARTITION_SIZE = PARTITION_SIZE_ROCM
@@ -173,6 +175,7 @@ def main(
                         k_scale,
                         v_scale,
                         None,
+                        ON_NAVI,
                     )
             else:
                 raise ValueError(f"Invalid version: {version}")
