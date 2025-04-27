df7b8db2f59c70e1f03810118890d352ee778704
Hui Liu <96135754+hliuca@users.noreply.github.com>
2025-04-26 23:43:34 -0500

    fix total_num_seq

--- csrc/rocm/attention.cu
+++ csrc/rocm/attention.cu
@@ -1815,9 +1815,9 @@ void paged_attention_custom_launcher(
 // clang-format off
 void paged_attention(
     torch::Tensor& out,         // [num_seqs, num_heads, head_size]
-    torch::Tensor& exp_sums,    // [num_seqs, num_heads, max_num_partitions]
+    torch::Tensor& exp_sums,    // [block_tables[0], num_heads, max_num_partitions]
     torch::Tensor& max_logits,  // [num_seqs, num_heads, max_num_partitions]
-    torch::Tensor& tmp_out,     // [num_seqs, num_heads, max_num_partitions, head_size]
+    torch::Tensor& tmp_out,     // [block_tables[0], num_heads, max_num_partitions, head_size]
     torch::Tensor& query,       // [num_seqs, num_heads, head_size]
     torch::Tensor& key_cache,   // [num_blocks, num_heads, head_size/x, block_size, x]
     torch::Tensor& value_cache, // [num_blocks, num_heads, head_size, block_size]
