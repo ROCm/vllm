aaf4937c66677bf6ee0083b1dfff686284e2fd5a
Mehmet Cagri Kaymak <mehmet.kaymak@amd.com>
2025-08-14 19:59:32 +0000

    update fp8 type based on the hardware/platform

--- tests/kernels/attention/test_triton_unified_attention.py
+++ tests/kernels/attention/test_triton_unified_attention.py
@@ -15,7 +15,7 @@ BLOCK_SIZES = [16, 32]
 
 DTYPES = [torch.float16, torch.bfloat16]
 QDTYPES = [None, torch.float8_e4m3fn] if not current_platform.is_rocm() else [
-    None, torch.float8_e4m3fnuz
+    None, current_platform.fp8_dtype()
 ]
 # one value large enough to test overflow in index calculation.
 # one value small enough to test the schema op check
