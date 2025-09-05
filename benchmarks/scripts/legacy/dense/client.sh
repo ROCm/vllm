69a18d247aaf5c3ff945341bef3d0d70a5ef78cf
zejunchen-zejun <zejun.chen@amd.com>
2025-09-05 08:45:45 +0800

    add seed and request-rate
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- benchmarks/scripts/legacy/dense/client.sh
+++ benchmarks/scripts/legacy/dense/client.sh
@@ -5,6 +5,7 @@ CONCUR=$2
 IN=$3
 OUT=$4
 NUM=$(expr $CONCUR \* 10)
+REQUEST_RATE=$(expr $CONCUR / 2)
 
 result_filename=CON${CONCUR}_IN${IN}_OUT${OUT}.json
 result_log=CON${CONCUR}_IN${IN}_OUT${OUT}.log
@@ -27,5 +28,7 @@ python3 ../../benchmark_serving.py \
     --result-dir ./bench_results/ \
     --result-filename ${result_filename} \
     --percentile-metrics "ttft,tpot,itl,e2el" \
+    --seed 123 \
+    --request-rate ${REQUEST_RATE} \
     2>&1 | tee ./bench_results/${result_log}
 
