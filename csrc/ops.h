20f6b2c8f297a74be719fb0e86446d2f7a262e95
Yutao Xu <xytpai@foxmail.com>
2025-09-08 23:20:07 +0800

    Update ops.h

--- csrc/ops.h
+++ csrc/ops.h
@@ -89,8 +89,7 @@ void convert_vertical_slash_indexes_mergehead(
 void rms_norm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& weight,
               double epsilon);
 
-void fused_add_rms_norm(torch::Tensor& out, torch::Tensor& input,
-                        torch::Tensor& residual_out, torch::Tensor& residual,
+void fused_add_rms_norm(torch::Tensor& input, torch::Tensor& residual,
                         torch::Tensor& weight, double epsilon);
 
 void apply_repetition_penalties_(torch::Tensor& logits,
