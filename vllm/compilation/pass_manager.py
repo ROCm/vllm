933c6b52c19f5ed347f71bb2398522a37470c971
Yutao Xu <xytpai@foxmail.com>
2025-09-25 00:46:27 +0800

    Update pass_manager.py

--- vllm/compilation/pass_manager.py
+++ vllm/compilation/pass_manager.py
@@ -14,6 +14,9 @@ if current_platform.is_cuda_alike():
 if current_platform.is_cuda():
     from .collective_fusion import AllReduceFusionPass, AsyncTPPass
 
+if current_platform.is_rocm():
+    from .rocm_fusion import ROCmFusionPass
+
 from .activation_quant_fusion import ActivationQuantFusionPass
 from .fix_functionalization import FixFunctionalizationPass
 from .inductor_pass import CustomGraphPass, InductorPass, get_pass_context
@@ -64,6 +67,8 @@ class PostGradPassManager(CustomGraphPass):
         if self.pass_config.enable_fusion:
             self.passes += [FusionPass.instance(config)]
             self.passes += [ActivationQuantFusionPass(config)]
+            if current_platform.is_rocm():
+                self.passes += [ROCmFusionPass(config)]
 
         if self.pass_config.enable_attn_fusion:
             self.passes += [AttnFusionPass(config)]
