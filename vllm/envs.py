c4858c4ed4cbd43e0709a40708b570e47752b4c6
jpvillam <juan.villamizar@amd.com>
2025-10-02 13:43:39 -0500

    add aiter moe a16w4 from #692

--- vllm/envs.py
+++ vllm/envs.py
@@ -110,6 +110,7 @@ if TYPE_CHECKING:
     VLLM_ROCM_USE_AITER_FP4_ASM_GEMM: bool = False
     VLLM_ROCM_USE_TRITON_ROPE: bool = False
     VLLM_ROCM_USE_AITER_FP8BMM: bool = True
+    VLLM_ROCM_USE_AITER_FUSED_MOE_A16W4: bool = False
     VLLM_ROCM_USE_SKINNY_GEMM: bool = True
     VLLM_ROCM_FP8_PADDING: bool = True
     VLLM_ROCM_MOE_PADDING: bool = True
@@ -968,6 +969,9 @@ environment_variables: dict[str, Callable[[], Any]] = {
     "VLLM_ROCM_USE_AITER_FP8BMM":
     lambda: (os.getenv("VLLM_ROCM_USE_AITER_FP8BMM", "True").lower() in
              ("true", "1")),
+
+    "VLLM_ROCM_USE_AITER_FUSED_MOE_A16W4":
+    lambda: bool(int(os.getenv("VLLM_ROCM_USE_AITER_FUSED_MOE_A16W4", "0"))),
 
     # use rocm skinny gemms
     "VLLM_ROCM_USE_SKINNY_GEMM":
