fbc0fbcf22aae6aa49bbad817c00ce459ba09d33
Xin Huang <Xin.Huang@amd.com>
2025-10-29 16:42:01 +0800

    Optimizes

--- .github/scripts/benchmark.sh
+++ .github/scripts/benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 # Usage function
 usage() {
-    echo "Usage: $0 <model_path> <output_path>"
+    echo "Usage: $0 <model_name> <output_path>"
     exit 1
 }
 
@@ -12,36 +12,13 @@ if [ $# -lt 2 ]; then
     usage
 fi
 
-model_path="$1"
+model_name="${1:-deepseekr1_ptpc_fp8}"
 output_path="$2"
 
-export VLLM_USE_V1=1
-export SAFETENSORS_FAST_GPU=1
-export VLLM_ROCM_USE_AITER=1
-export VLLM_ROCM_USE_AITER_MOE=1
-export VLLM_USE_TRITON_FLASH_ATTN=0
-export NCCL_DEBUG=WARN
-export VLLM_RPC_TIMEOUT=1800000
-export VLLM_ROCM_USE_AITER_MHA=0
-export VLLM_ROCM_USE_TRITON_ROPE=1 # for accuracy
-export VLLM_ROCM_USE_AITER_FUSION_SHARED_EXPERTS=1 # for accuracy, performance may not be good in some cases
-
-# Set profiling options
-export VLLM_TORCH_PROFILER_DIR="deepseek_in3k_out1k"
-export VLLM_TORCH_PROFILER_WITH_STACK=1
-export VLLM_TORCH_PROFILER_RECORD_SHAPES=1
-
+# Launch vLLM server
 echo "================================================"
 echo "========== LAUNCHING vLLM SERVER =============="
-vllm serve "$model_path" \
-    --tensor-parallel-size 8 \
-    --max-num-batched-tokens 32768 \
-    --trust-remote-code \
-    --no-enable-prefix-caching \
-    --disable-log-requests \
-    --compilation-config '{"cudagraph_mode": "FULL_AND_PIECEWISE"}' \
-    --gpu_memory_utilization 0.9 \
-    --block-size 1 &
+evaluation/launch_${model_name}.sh &
 
 VLLM_PID=$!
 
@@ -51,15 +28,15 @@ MAX_RETRIES=60
 RETRY_INTERVAL=60
 for ((i=1; i<=MAX_RETRIES; i++)); do
     if curl -s http://localhost:8000/v1/completions -o /dev/null; then
-        echo "========== vLLM server is up. =========="
+        echo "vLLM server is up."
         break
     fi
-    echo "========== Waiting for vLLM server to be ready... ($i/$MAX_RETRIES) =========="
+    echo "Waiting for vLLM server to be ready... ($i/$MAX_RETRIES)"
     sleep $RETRY_INTERVAL
 done
 
 if ! curl -s http://localhost:8000/v1/completions -o /dev/null; then
-    echo "========== vLLM server did not start after $((MAX_RETRIES * RETRY_INTERVAL)) seconds. =========="
+    echo "vLLM server did not start after $((MAX_RETRIES * RETRY_INTERVAL)) seconds."
     kill $VLLM_PID
     exit 1
 fi
@@ -81,6 +58,7 @@ lm_eval \
     --batch_size 100 \
     --output_path "$output_path"
 
+EXIT_CODE=$?
 echo "================================================"
 echo "========== EXIT CODE: $EXIT_CODE ==============="
 if [ $EXIT_CODE -eq 0 ]; then
