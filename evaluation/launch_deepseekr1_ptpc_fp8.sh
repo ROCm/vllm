e0f12f3fc650184107f348b8c391f653c9b8ab7c
wuhuikx <wuhui_csu@163.com>
2025-11-08 23:02:44 +0800

    Update launch_deepseekr1_ptpc_fp8.sh

--- evaluation/launch_deepseekr1_ptpc_fp8.sh
+++ evaluation/launch_deepseekr1_ptpc_fp8.sh
@@ -10,9 +10,10 @@ export VLLM_ROCM_USE_AITER=1
 #export VLLM_ROCM_USE_TRITON_ROPE=1 # add for acc
 export VLLM_ROCM_USE_AITER_FUSION_SHARED_EXPERTS=1 # add for acc, perf is not good for some cases
 
-export VLLM_TORCH_PROFILER_DIR="deepseek_in3k_out1k"
+export VLLM_CUSTOM_SCOPES_FOR_PROFILING=1
 export VLLM_TORCH_PROFILER_WITH_STACK=1
 export VLLM_TORCH_PROFILER_RECORD_SHAPES=1
+export VLLM_TORCH_PROFILER_DIR="deepseek_in3k_out1k"
 
 # original weight  https://huggingface.co/EmbeddedLLM/deepseek-r1-FP8-Dynamic
 model_path="/mnt/raid0/zhangguopeng/deepseek-r1-FP8-Dynamic/"
