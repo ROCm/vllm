d85c67864d4da25e8b8a0b4ca8ce7339b0b93fdd
ZhiweiYan-96 <zhiwei.yan@amd.com>
2025-12-05 02:50:29 +0000

    enable wmxfp4&amxfp4 in quark_moe

--- vllm/model_executor/layers/fused_moe/config.py
+++ vllm/model_executor/layers/fused_moe/config.py
@@ -344,6 +344,10 @@ class FusedMoEQuantConfig:
     @property
     def use_mxfp4_w4a16(self) -> bool:
         return self._a1.dtype is None and self._w1.dtype == "mxfp4"
+    
+    @property
+    def use_mxfp4_w4a4(self) -> bool:
+        return self._a1.dtype == "mxfp4" and self._w1.dtype == "mxfp4"
 
     @property
     def use_nvfp4_w4a4(self) -> bool:
