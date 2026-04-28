63c18b91f4984d02ca89e58fe7269564adb95366
whx-sjtu <xiaowang990929@gmail.com>
2026-04-28 10:15:56 +0000

    remove _old_forward
    Signed-off-by: whx-sjtu <xiaowang990929@gmail.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -4044,7 +4044,6 @@ class GPUModelRunner(
                 batch_descriptor=batch_desc,
                 ubatch_slices=ubatch_slices_padded,
                 slot_mapping=slot_mappings,
-                    additional_kwargs={"req_ids": self.input_batch.req_ids.copy()},
                 skip_compiled=has_encoder_input,
             ),
             record_function_or_nullcontext("gpu_model_runner: forward"),
@@ -5537,7 +5536,6 @@ class GPUModelRunner(
                     batch_descriptor=batch_desc,
                     ubatch_slices=ubatch_slices_padded,
                     slot_mapping=slot_mappings,
-                    additional_kwargs={"req_ids": self.input_batch.req_ids.copy()},
                 ),
             ):
                 outputs = self.model(
